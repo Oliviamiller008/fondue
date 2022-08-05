e2f4473ff95befd670376a629736efd42883caf5
oliviamiller <106617921+oliviamiller@users.noreply.github.com>
2022-08-05 15:56:42 -0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,60 @@
+Python 3.10.4 (v3.10.4:9d38120e33, Mar 23 2022, 17:29:05) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+import asyncio
+
+from viam.robot.client import RobotClient
+from viam.rpc.dial import Credentials, DialOptions
+from viam.components.sensor import Sensor
+from kasa import Discover, SmartPlug
+
+async def client():
+  creds = Credentials(
+      type='robot-location-secret',
+      payload='lauqrt4op4x6ubhji867wue0qsnqdq76x00ljfv7vcoyq7mi')
+  opts = RobotClient.Options(
+      refresh_interval=0,
+      dial_options=DialOptions(credentials=creds)
+  )
+  async with await RobotClient.at_address(
+    'tempbot-main.rdt5n4brox.local.viam.cloud:8080',
+    opts) as robot:
+      print('Resources:')
+      print(robot.resource_names)
+
+      sensor = Sensor.from_robot(robot, "sensor")
+
+
+      # getting device address
+      # devices = await Discover.discover()
+      # for addr, dev in devices.items():
+      #     await dev.update()
+      #     print(addr)
+      #     print(dev)
+
+      plug = SmartPlug('10.1.11.221')
+      await plug.update()
+
+
+      while(1):
+        temp = await sensor.get_readings() 
+        print(temp[0])
+        ison = plug.is_on
+
+        if(temp[0] > 62 and ison):
+          await plug.turn_off()
+          await plug.update()
+          print("turning off")
+        
+        if(temp[0] < 55 and not ison):
+          await plug.turn_on()
+          await plug.update()
+          print("turning on")
+
+        await asyncio.sleep(5)
+
+
+
+
+
+if __name__ == '__main__':
+  asyncio.run(client())
